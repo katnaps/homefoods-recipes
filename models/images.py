ff29c998141f5f0fd1d0153db2efbb0dea44870b
Azri <59246759+katnaps@users.noreply.github.com>
2020-11-05 18:33:47 +0800

    add api for profile_image upload

---
+++ models/images.py
@@ -0,0 +1,14 @@
+import peewee as pw
+from models.base_model import BaseModel
+from models.user import User
+from playhouse.hybrid import hybrid_property
+
+
+class Image(BaseModel):
+    user = pw.ForeignKeyField(User, backref="images", on_delete="CASCADE")
+    image_url = pw.TextField(null=False)
+
+    @hybrid_property
+    def full_image_url(self):
+        from app import app
+        return app.config.get("AWS_S3_DOMAIN") + self.image_url
