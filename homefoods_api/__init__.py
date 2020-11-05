abafe1107b32626ae68461ef9a79f02c7614f7e9
Eric <looicen66@gmail.com>
2020-11-05 21:16:51 +0800

    api/users

--- homefoods_api/__init__.py
+++ homefoods_api/__init__.py
@@ -6,7 +6,15 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 ## API Routes ##
 from homefoods_api.blueprints.login.views import login_api_blueprint
 from homefoods_api.blueprints.users.views import users_api_blueprint
+from homefoods_api.blueprints.images.views import images_api_blueprint
+from homefoods_api.blueprints.comments.views import comments_api_blueprint
+
+
 
 
 app.register_blueprint(login_api_blueprint, url_prefix='/api/v1/login')
 app.register_blueprint(users_api_blueprint, url_prefix='/api/v1/users')
+app.register_blueprint(images_api_blueprint, url_prefix='/api/v1/images')
+app.register_blueprint(comments_api_blueprint, url_prefix='/api/v1/comments')
+
+
--- homefoods_api/blueprints/users/views.py
+++ homefoods_api/blueprints/users/views.py
@@ -7,6 +7,7 @@ users_api_blueprint = Blueprint('users_api',
                              __name__,
                              template_folder='templates')
 
+# we could expand on the errors later
 @users_api_blueprint.route('/', methods=['POST'])
 def create():
     data = request.json
@@ -19,9 +20,34 @@ def create():
             "token": token,
             "message": "Successfully created a user and signed in.",
             "status": "success",
-            "id": user.id,
-            "username": user.username
+            "user": {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email
+            }
         })
     else:
         return jsonify({"Error": "Invalid credentials"})
 
+
+# get all users
+# maybe include "profileImage"
+@users_api_blueprint.route('/', methods=['GET'])
+def index():
+    users = User.select()
+    return jsonify([{"id": user.id, "email": user.email, "username": user.username} for user in users])
+
+# retrieve information of currently LOGGED-IN user
+# please include profile_picture
+@users_api_blueprint.route('/me', methods=['GET'])
+@jwt_required
+def me():
+    user_id = get_jwt_identity()
+    # what the line below means
+    user = User.get_or_none(User.id == user_id)
+    if user:
+        return jsonify({
+            "id": user.id,
+            "username": user.username,
+            "email": user.email
+        })
